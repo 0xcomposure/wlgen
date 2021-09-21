aca631bfef26587ca9ee6d5b00b64d8179df7c74
0xComposure <0xComposure@users.noreply.github.com>
2021-09-21 16:01:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+**wlgen**
+============================
+
+Wordlist generator for a target.
+
+**Usage**
+
+``--target -T``  Enter the name of the target.
+
+``--output -O`` Output file.
+
+``--complete -C`` Add more causistry.
+
+``--keywords -K`` List of keywords to add, separated by comma and no space between each keyword. Example: 'example,keyword,test,word'
+
+``--lang, -L`` Language of the target. Example: '-L eng'
---
+++ wlgen.py
@@ -0,0 +1,162 @@
+import json
+import sys
+import signal
+import argparse
+from datetime import date
+
+days = []
+months = []
+seasons = []
+keywords = []
+languages = []
+lang = ''
+target = ''
+filename = 'wordlist.txt'
+resourcespath = 'resources/'
+currentyear = date.today().year
+complete = False
+minyear = 1970
+
+with open(resourcespath + 'languages.json') as languagesjson:
+    languages = json.load(languagesjson)
+
+parser = argparse.ArgumentParser(description='Wordlist Generator.')
+parser.add_argument('--target',
+                    '-T',
+                    help = "Enter the name of the target: "
+                         + "-T targetcorp",
+                    required = True,
+                    nargs = 1,
+                    type = str)
+
+parser.add_argument('--output',
+                    '-O',
+                    help = "Output file: "
+                         + "-O wordlist.txt",
+                    nargs = 1,
+                    type = str)
+
+parser.add_argument('--complete',
+                    '-C',
+                    help = 'Add more causistry:',
+                    action = 'store_true')
+
+parser.add_argument('--keywords',
+                    '-K',
+                    help = 'List of keywords to add, separated by comma and no space between each keyword:'
+                         + '-K example,keyword,test,word',
+                    nargs = 1,
+                    type = str)
+
+parser.add_argument('--lang',
+                    '-L',
+                    help = 'Language of the target:\tEnglish:eng, Spanish:spa, Vietnamese:vie',
+                    required = True,
+                    nargs = 1,
+                    type = str)
+
+args = parser.parse_args()
+
+if args.target is not None:
+    target = str(args.target[0])
+    print('Target: ' + target + '\n')
+else:
+    exit('It requires the target')
+
+if args.output is not None:
+    filename = args.output[0]
+    print('Output file: ' + filename + '\n')
+
+if args.complete is True:
+    complete = True
+
+if args.keywords is not None:
+    try:
+        keywords = str(args.keywords[0]).split(',')
+    except Exception as e:
+        print('The keywords must be separated by comma and no space between each keyword.')
+        exit(str(e))
+
+if args.lang is not None:
+    try:
+        lang = str(args.lang[0])
+        if lang in languages['languages']:
+            print('Language for the target: ' + lang + '\n')
+    except Exception as e:
+        print('The language code is not valid')
+        exit(str(e))
+
+else:
+    exit('It requires the language for the target')
+
+file = open(filename, 'w+')
+
+
+def exit(msg):
+    parser.print_help(sys.stderr)
+    print(msg, file=sys.stderr)
+
+
+def sigint_handler(signal, frame):
+    print ('Task interrupted...')
+    sys.exit(0)
+signal.signal(signal.SIGINT, sigint_handler)
+
+
+
+def writetofile(v, n = 0):
+    if n:  
+        file.write(v + str(n) + '\n')
+        file.write(v[0].upper() + v[1:] + str(n) + '\n')
+        file.write(v.upper() + str(n) + '\n')
+        if complete:
+            file.write(str(n) + v + '\n')
+            file.write(str(n) + v[0].upper() + v[1:] + '\n')
+            file.write(str(n) + v.upper() + '\n')
+    else:
+        file.write(v + '\n')
+        file.write(v[0].upper() + v[1:] + '\n')
+        file.write(v.upper() + '\n')
+        
+
+def getjson(jsondata, typedata):
+    return json.load(jsondata)[typedata]['languages'][lang]
+
+with open(resourcespath + 'days.json') as days_json:
+    days = getjson(days_json, 'days')
+
+with open(resourcespath + 'months.json') as months_json:
+    months = getjson(months_json, 'months')
+
+with open(resourcespath + 'seasons.json') as seasons_json:
+    seasons = getjson(seasons_json, 'seasons')
+
+for d in days:
+    writetofile(d)
+    for dn in range(1, 31):
+        writetofile(d, dn)
+
+if complete:
+    minyear = 1
+
+for m in months:
+    writetofile(m)
+    for mn in range(minyear, currentyear):
+        writetofile(m, mn)
+
+for s in seasons:
+    writetofile(s)
+    for sn in range(minyear, currentyear):
+        writetofile(s, sn)
+
+writetofile(target)
+for tn in range(minyear, currentyear):
+    writetofile(target, tn)
+
+if len(keywords):
+    for k in keywords:
+        writetofile(k)
+        for kn in range(minyear, currentyear):
+            writetofile(k, kn)
+print('Wordlist saved in: ' + filename);
+file.close()
